faf241a6cf0942a8b5cf2e147b2f0b8716cfdb35
qq343893081 <qq343893081@fa3e71d5-1eb2-433f-8710-6ba5aafa071d>
2016-05-09 07:26:01 +0000

    增加微信 config

---
+++ trunk/Weichat/MessageHandle/WechatParamList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeiChatMessageHandle
+{
+    public class WechatParamList
+    {
+        public const string APP_ID = "wx5c2b91a9bbef68b4";
+        public const string APP_SECRET = "e895093b0a7227eac53199d8bcf4e031";
+
+    }
+}
