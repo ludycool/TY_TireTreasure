1f4a3dabad90036e2a09d5d1bed4a78cca429a93
qq343893081 <qq343893081@fa3e71d5-1eb2-433f-8710-6ba5aafa071d>
2016-05-09 07:26:01 +0000

    增加微信 config

---
+++ Weichat/MessageHandle/GetJsParam.cs
@@ -0,0 +1,50 @@
+﻿using BH.Community.tools.ToolsHelper;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeiChatMessageHandle
+{
+    public class GetJsParam
+    {
+        private static HttpUtil request = new HttpUtil();
+        private static JObject obj;
+        private static string getAccessToken()
+        {
+            string url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + WechatParamList.APP_ID + "&secret=" + WechatParamList.APP_SECRET + "";
+            string result = request.GetRequest(url);
+            obj = JObject.Parse(result);
+            string token = obj["access_token"].ToString();
+            return token;
+        }
+        public static string getJsApiTicket()
+        {
+            string accessToken = getAccessToken();
+            string url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + accessToken + "&type=jsapi";
+            string result = request.GetRequest(url);
+            obj = JObject.Parse(result);
+            string ticket = obj["ticket"].ToString();
+            return ticket;
+        }
+
+        public static string generateNoncestr(int length = 16)
+        {
+            //string noncestr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            //string str = "";
+            //for (int i = 0; i < noncestr.Length; i++)
+            //{
+            //    Random ran = new Random();
+            //    str = noncestr.Substring(ran.Next(0, noncestr.Length - 1), 1);
+            //}
+            //return str;
+            return Guid.NewGuid().ToString().Replace("-", "").Substring(0, length > 32 ? 32 : length);
+        }
+        public static int generateTimeStamp()//创建时间戳
+        {
+            DateTime dateStart = new DateTime(1970, 1, 1, 0, 0, 0);//格林威治时间1970，1，1，0，0，0
+            return (int)(DateTime.Now - dateStart).TotalSeconds;
+        }      
+    }
+}
