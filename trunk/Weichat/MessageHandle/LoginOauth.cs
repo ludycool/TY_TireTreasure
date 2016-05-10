55585855f249c6464fdaf96f044426b05cb9cf27
qq343893081 <qq343893081@fa3e71d5-1eb2-433f-8710-6ba5aafa071d>
2016-05-10 10:00:19 +0000

    优化代码

---
+++ trunk/Weichat/MessageHandle/LoginOauth.cs
@@ -0,0 +1,51 @@
+﻿using BH.Community.tools.ToolsHelper;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeiChatMessageHandle.OpenId
+{
+    public class OauthLogin
+    {
+        public const int OPEND_ID = 0;
+        public const int NICK_NAME = 1;
+        public const int HEAD_IMG_URL = 2;
+        static HttpUtil request = new HttpUtil();
+        static JObject obj;
+        public static string[] getOpenId(string code)
+        {
+
+            string[] strArray = new String[3];
+            if (code != null)
+            {
+                string url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + WechatParamList.APP_ID + "&secret=" + WechatParamList.APP_SECRET + "&code=" + code + "&grant_type=authorization_code";
+                string result = request.GetRequest(url);
+
+                obj = JObject.Parse(result);
+                string token = obj["access_token"].ToString();
+                string openid = obj["openid"].ToString();
+                string refreshToken = obj["refresh_token"].ToString();
+                //str = "access_token:" + token + "\n" + "openid:"+openid;
+
+                string userResult = getUserInfo(token, openid);
+                obj = JObject.Parse(userResult);
+                string nickName = obj["nickname"].ToString();
+                string headImgUrl = obj["headimgurl"].ToString();
+
+                strArray[OPEND_ID] = openid;
+                strArray[NICK_NAME] = nickName;
+                strArray[HEAD_IMG_URL] = headImgUrl;
+            }
+            return strArray;
+        }
+
+        private static string getUserInfo(string token, string openid)
+        {
+            string url = "https://api.weixin.qq.com/sns/userinfo?access_token=" + token + "&openid=" + openid + "&lang=zh_CN";
+            string result = request.GetRequest(url);
+            return result;
+        }
+    }
+}
--- trunk/Weichat/ZAppUI/Controllers/UserController.cs
+++ trunk/Weichat/ZAppUI/Controllers/UserController.cs
@@ -6,6 +6,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using e3net.BLL.Money;
+using WeiChatMessageHandle;
+using System.Web.Security;
+using WeChatJsSdk.SdkCore;
+using System.Web.Script.Serialization;
+using BH.PalmHosp.tools.ToolsHelper;
+using TZHSWEET.Common;
+using e3net.Mode.Base;
+using e3net.tools;
 namespace ZAppUI.Controllers
 {
     public class UserController : BaseController
@@ -14,25 +22,20 @@ namespace ZAppUI.Controllers
         // GET: /User/
         public ActionResult Index()
         {
-            showUserInfo();
-
+            getNickName();
+            getBalance();  
             return View();
         }
-        private void showUserInfo()
-        {
-            getNickName();
-            getBalance();
-        }
+       
         //显示用户昵称头像
         private void getNickName()
         {
             AppUserInfoBiz userInfoBiz = new AppUserInfoBiz();
             string openId = GetUData.OpenId;
 
-            DataSet result = userInfoBiz.ExecuteSqlToDataSet("EXEC	[TireTreasureDB].[dbo].[proc_SearchUserInfo] '" + openId + "'");
+            DataSet result = userInfoBiz.ExecuteSqlToDataSet("EXEC	[TireTreasureDB].[dbo].[proc_GetUserInfo] '" + openId + "'");
             if (result.Tables.Count > 0 && result.Tables[0].Rows.Count > 0)
             {
-
                 Object nickName = result.Tables[0].Rows[0]["Nickname"];
                 Object headImgUrl = result.Tables[0].Rows[0]["ImgeUrl"];
 
@@ -44,22 +47,18 @@ namespace ZAppUI.Controllers
         //显示用户余额
         private void getBalance()
         {
-
-
             string openId = GetUData.OpenId;
 
             BalanceBiz balanceBiz = new BalanceBiz();
-            DataSet result = balanceBiz.ExecuteSqlToDataSet("EXEC [TireMoneyDB].[dbo].[proc_CheckBalanceInfo] '" + openId + "'");
-            if (result.Tables.Count > 0 && result.Tables[0].Rows.Count > 0)
+            DataSet isHaveBalance = balanceBiz.ExecuteSqlToDataSet("EXEC [TireMoneyDB].[dbo].[proc_CheckBalanceInfo] '" + openId + "'");
+            if (!(isHaveBalance.Tables.Count > 0) && !(isHaveBalance.Tables[0].Rows.Count > 0))
             {
+                createUserBalanceInfo(openId);
+               
+            }
+                DataSet result = balanceBiz.ExecuteSqlToDataSet("EXEC [TireMoneyDB].[dbo].[proc_CheckBalanceInfo] '" + openId + "'");
                 Object balance = result.Tables[0].Rows[0]["AMneys"];
                 ViewBag.balance = balance.ToString();
-            }
-            else
-            {
-
-                createUserBalanceInfo(openId);
-            }
         }
         private void createUserBalanceInfo(string openId)
         {
@@ -67,6 +66,108 @@ namespace ZAppUI.Controllers
             Guid baId = Guid.NewGuid();
             DateTime now = DateTime.Now;
             balanceBiz.ExecuteSqlToDataSet("EXEC [TireMoneyDB].[dbo].[proc_AddUserBalanceInfo] '" + openId + "','" + now + "','" + now + "'");
+        }
+
+        
+        //private string getUrl()
+        //{
+        //    return "http://" + Request.Url.Host + Request.Path;          
+        //}
+
+        //string timestamp = GetJsParam.generateTimeStamp().ToString();
+        //string noncestr = GetJsParam.generateNoncestr();    
+
+        //public string generateSignature(string timestamp, string noncestr)
+        //{
+        //    string jsapi_ticket = GetJsParam.getJsApiTicket();
+        //    //string jsapi_ticket = "bxLdikRXVbTPdHSM05e5ux0lWAqibUJZA_EYsbIYfhEZKc8OyZW1m4eLYglBLgCKC0JGvz964nia_9GtnPEMVw";
+        //    string url = getUrl();
+        //    ViewBag.url = url;
+        //    string beforeSignature = "jsapi_ticket=" + jsapi_ticket + "&noncestr=" + noncestr + "&timestamp=" + timestamp + "&url=" + url + "";
+           
+        //    string generatedSignature = FormsAuthentication.HashPasswordForStoringInConfigFile(beforeSignature, "SHA1");
+        //    return generatedSignature.ToLower();
+        //}
+        //public void signPackge()
+        //{
+
+        //    JSSDK sdk = new JSSDK(WechatParamList.APP_ID, WechatParamList.APP_SECRET, true);
+        //    JsApiEnum jsApilist = (JsApiEnum)0x3ffffffff;
+
+        //    SignPackage config = sdk.GetSignPackage(jsApilist);
+        //    JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+        //    ViewBag.config = serializer.Serialize(config);
+
+        //    //ViewBag.appid = WechatParamList.APP_ID;
+        //    //ViewBag.timestamp = timestamp;
+        //    //ViewBag.nonceStr = noncestr;
+        //    //ViewBag.signature = generateSignature(timestamp, noncestr);
+        //}
+
+
+
+        public ActionResult recommend()
+        {
+            //JSSDK sdk = new JSSDK(WechatParamList.APP_ID, WechatParamList.APP_SECRET, true);
+            //JsApiEnum jsApilist = (JsApiEnum)0x3ffffffff;
+
+            //SignPackage config = sdk.GetSignPackage(jsApilist);
+            //JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            //ViewBag.config = serializer.Serialize(config);
+
+            shareUrl();
+
+            return View();
+        }
+        
+        public string getUserId()
+        {
+            string loginName = null;
+
+            UserBiz userBiz = new UserBiz();
+            DataSet result = userBiz.ExecuteSqlToDataSet("EXEC [TireTreasureDB].[dbo].[proc_GetUserIdByWeiXinID] '" + GetUData.OpenId + "'");
+            if ((result.Tables.Count > 0) && (result.Tables[0].Rows.Count > 0))
+            {
+                Object phone = result.Tables[0].Rows[0]["UserId"];
+                loginName = phone.ToString();
+            }
+            return loginName;          
+        }
+
+        string Key = "12345678";
+
+        public void shareUrl()
+        {           
+            string userId=XXTEAHelper.Encrypt(getUserId(),Key);
+            string url = "/user/encoderUrl?param=" + userId + "&type=recommend";
+            ViewBag.url = url;          
+        }
+        public void encoderUrl()
+        {
+            string userId =FilterTools.FilterSpecial( XXTEAHelper.Decrypt(Request["param"], Key));
+            string type = FilterTools.FilterSpecial(Request["type"]);
+
+            if (type.Equals("recommend"))
+            {
+                UserBiz userBiz = new UserBiz();
+                DataSet result = userBiz.ExecuteSqlToDataSet("EXEC [TireTreasureDB].[dbo].[proc_GetUserIdByWeiXinID] '" + userId + "'");
+                if ((result.Tables.Count > 0) && (result.Tables[0].Rows.Count > 0))
+                {
+
+                    //ReferencesBiz referencesBiz = new ReferencesBiz();
+                    //References references = new References();
+                    //references.UserId = userId;
+                    string redirect_uri = "http://test.luntaibaobao.com/register";
+                    //    string state = "test";
+                    //    string url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + APP_ID + "&redirect_uri=" + redirect_uri + "&response_type=code&scope=snsapi_base&state=" + state + "#wechat_redirect";
+                    //    return Redirect(url);
+                }
+                return "";
+            }
+            else
+            {
+                //TODO
+            }
         }
     }
 }
