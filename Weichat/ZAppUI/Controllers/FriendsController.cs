60c0f12c6a86febd11b90339de9755732ad1b861
qq343893081 <qq343893081@fa3e71d5-1eb2-433f-8710-6ba5aafa071d>
2016-05-18 08:36:12 +0000

    好友列表

--- Weichat/ZAppUI/Controllers/FriendsController.cs
+++ Weichat/ZAppUI/Controllers/FriendsController.cs
@@ -1,6 +1,7 @@
 ﻿using e3net.BLL.Base;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,7 +20,21 @@ namespace ZAppUI.Controllers
             //isRegister(controller,action);
 
             FriendsBiz friendsBiz=new FriendsBiz();
-            friendsBiz.ExecuteSqlToDataSet("");
+            DataSet result=friendsBiz.ExecuteSqlToDataSet("EXEC	[TireTreasureDB].[dbo].[proc_GetUserInfoBy_v_FriendsList] '" + GetUData.OpenId + "'");
+            if (result.Tables[0].Rows.Count>0)
+            {
+                List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
+                for (int count = 0; count < result.Tables[0].Rows.Count; count++)
+                {
+                    Dictionary<string, string> dictionary = new Dictionary<string, string>();
+                    dictionary.Add("Nickname", result.Tables[0].Rows[count]["Nickname"].ToString());
+                    dictionary.Add("ImgeUrl", result.Tables[0].Rows[count]["ImgeUrl"].ToString());
+                    dictionary.Add("AddTime", result.Tables[0].Rows[count]["AddTime"].ToString());
+                    list.Add(dictionary);
+                }
+                ViewBag.friendList = list;
+            }
+            
             return View();
         }
 
