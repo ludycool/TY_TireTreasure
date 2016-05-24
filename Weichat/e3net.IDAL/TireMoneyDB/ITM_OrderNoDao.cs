2b1ae487444c308a0c18697610f57a8a7a72a640
qq343893081 <qq343893081@fa3e71d5-1eb2-433f-8710-6ba5aafa071d>
2016-05-24 03:21:08 +0000

    增加订单编号实体

---
+++ Weichat/e3net.IDAL/TireMoneyDB/ITM_OrderNoDao.cs
@@ -0,0 +1,13 @@
+﻿using e3net.Mode.TireMoneyDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace e3net.IDAL.TireMoneyDB
+{
+    public interface ITM_OrderNoDao : IBaseDao<TM_OrderNo>
+    {
+    }
+    
+}
