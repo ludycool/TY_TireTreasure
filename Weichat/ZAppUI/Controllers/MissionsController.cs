2dc8377400fdea920621ad7af33ea20f3588b488
ludycool <ludycool@fa3e71d5-1eb2-433f-8710-6ba5aafa071d>
2016-05-26 09:35:17 +0000

    任务图片

---
+++ Weichat/ZAppUI/Controllers/MissionsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WeChatJsSdk.SdkCore;
+using WeiChatMessageHandle;
+
+namespace ZAppUI.Controllers
+{
+    public class MissionsController : JsonNetController
+    {
+        //
+        // GET: /Missions/
+
+        public ActionResult Index()
+        {
+
+            JSSDK sdk = new JSSDK(WechatParamList.APP_ID, WechatParamList.APP_SECRET, true);
+            JsApiEnum jsApilist = (JsApiEnum)0x3ffffffff;
+
+            SignPackage config = sdk.GetSignPackage(jsApilist);
+            JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            ViewBag.config = serializer.Serialize(config);
+            return View();
+        }
+
+    }
+}
